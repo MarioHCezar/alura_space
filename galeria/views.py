3a37bdd0c3943e0de8faa562c37b8ca8627ed83d
MarioHCezar <mariohcezar@gmail.com>
2024-07-16 08:10:57 -0300

    teste com uso de dicionario de dados

--- galeria/views.py
+++ galeria/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
-# Create your views here.
 
+# render permite envio de dados
 def index(request):
-    return render(request, 'index.html')
+    dados = {
+        1: {'nome': 'Nebulosa de Carina', 
+            'legenda': 'webtelescope.org / NASA / James Webb'},
+        2: {'nome': 'Galaxia NGC 1079', 
+            'legenda': 'nasa.org / NASA / Hubble'}
+    }
+
+    return render(request, 'index.html', {'cards': dados})
+
 
 def imagem(request):
     return render(request, 'imagem.html')
